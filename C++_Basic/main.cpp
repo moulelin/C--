da3e6734684a15a57e703ce0cb43d37dd92685aa
Lin Moule <moulecs@gmail.com>
2022-02-08 12:09:16 +0100

    Initial Commit

---
+++ C++_Basic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++_Basic
+//
+//  Created by moule on 2022/2/8.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
